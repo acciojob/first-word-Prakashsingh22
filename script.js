ea6479acb4d6df01c095d12117b59ed3433c6956
Prakashsingh22 <109896446+Prakashsingh22@users.noreply.github.com>
2025-10-26 13:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function firstWord(s) {
   // your code here
+	s = s.trim();
+	if(s === "") return "";
+	let firstSpace = s.indexOf(" ");
+	if(firstSpace === -1)return s; 
+	return s.substring(0,firstSpace); 
 }
 
 // Do not change the code below
